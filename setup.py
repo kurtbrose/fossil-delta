62183958fd4279dbbc9ee2e3265105560460a1e3
Kurt Rose <kurt@kurtrose.com>
2017-12-06 14:26:27 -0800

    better setup.py (remove cython dependency)

--- setup.py
+++ setup.py
@@ -1,7 +1,22 @@
 from distutils.core import setup
-from Cython.Build import cythonize
+from distutils.extension import Extension
+import os
+
+USE_CYTHON = os.environ.get('USE_CYTHON')
+
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [Extension("fossil_delta", ["src/fossil_delta" + ext])]
+
+if USE_CYTHON:
+	from Cython.Build import cythonize
+	extensions = cythonize(extensions)
 
 setup(
-  name = 'fossil-delta',
-  ext_modules = cythonize("src/fossil_delta.pyx"),
+  name='fossil-delta',
+  version='17.12.0',
+  description='thin wrapper of string delta functions from fossil SCM',
+  url='https://github.com/kurtbrose/fossil-delta',
+  platforms='any',
+  ext_modules=extensions,
 )
